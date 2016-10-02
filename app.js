04196fa8137ba4d132edd26e5779ea2b19d1020e
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-10-02 12:57:04 -1000

    added completed button

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log('test');
+
+//add something to the to-do list function
+
+function addFunc(){
+
+}
+
+//list active items in to-do list that are not completed
+
+function activeFunc(){
+
+}
+
+// show everything, Active and completed lists.
+
+function allFunc(){
+
+}
+
+//show completed things
+
+function completedFunc(){
+
+}
