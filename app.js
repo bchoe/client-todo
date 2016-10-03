42f3b3ac5bc411c300c46fd5f01f27ddda64fa21
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-10-02 17:26:30 -1000

    add function added

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 console.log('test');
 
+//what i type in
+let input = document.getElementById('inputBox');
+
+//to-do list container
+let list = document.getElementById('toDoList');
+
 //add something to the to-do list function
 
 function addFunc(){
 
+  //to-do list is added in here
+  let newItem = document.createElement('div');
+  // adds new value to innerHTML
+  newItem.innerHTML = input.value;
+  //adds created newItem to list
+  list.appendChild(newItem);
+
+  console.log(list);
+  console.log(newItem);
 }
 
 //list active items in to-do list that are not completed
